7d18bf7b01bfcf4a63bd3eb9f32d9708daa84048
David Wu <fastbyte22@protonmail.com>
2016-08-18 15:13:40 +0200

    Winscreen

---
+++ app/src/main/java/com/jimdo/hanhan/lightsout/WinScreen.java
@@ -0,0 +1,39 @@
+package com.jimdo.hanhan.lightsout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class WinScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lose_screen);
+
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.layout);
+
+        Button btnTryAgain = (Button) findViewById(R.id.btnTryAgain);
+        Button btnHome = (Button) findViewById(R.id.btnHome);
+
+        btnTryAgain.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainLevel.class);
+                startActivity(intent);
+            }
+        });
+
+        btnHome.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
