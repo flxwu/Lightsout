a35340ac6c04188e3558916a25402a9c32a1fb45
David Wu <fastbyte22@protonmail.com>
2016-08-18 10:24:53 +0200

    changed extending activity

--- app/src/main/java/com/jimdo/hanhan/lightsout/LoseScreen.java
+++ app/src/main/java/com/jimdo/hanhan/lightsout/LoseScreen.java
@@ -1,13 +1,32 @@
 package com.jimdo.hanhan.lightsout;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class LoseScreen extends AppCompatActivity {
+public class LoseScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lose_screen);
+
+
+        RelativeLayout layout=(RelativeLayout) findViewById(R.id.layout);
+
+        Button btnTryAgain = (Button) findViewById(R.id.btnTryAgain);
+        btnTryAgain.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent( getApplicationContext(), MainLevel.class  );
+                startActivity(intent);
+            }
+        });
     }
 }
