632452bf6920acf25632711995a68dd906612e12
David Wu <fastbyte22@protonmail.com>
2016-08-18 10:46:53 +0200

    onclicklistener for btnHome

--- app/src/main/java/com/jimdo/hanhan/lightsout/LoseScreen.java
+++ app/src/main/java/com/jimdo/hanhan/lightsout/LoseScreen.java
@@ -18,13 +18,23 @@ public class LoseScreen extends Activity {
         setContentView(R.layout.activity_lose_screen);
 
 
-        RelativeLayout layout=(RelativeLayout) findViewById(R.id.layout);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.layout);
 
         Button btnTryAgain = (Button) findViewById(R.id.btnTryAgain);
+        Button btnHome = (Button) findViewById(R.id.btnHome);
+
         btnTryAgain.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent( getApplicationContext(), MainLevel.class  );
+                Intent intent = new Intent(getApplicationContext(), MainLevel.class);
+                startActivity(intent);
+            }
+        });
+
+        btnHome.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Home.class);
                 startActivity(intent);
             }
         });
