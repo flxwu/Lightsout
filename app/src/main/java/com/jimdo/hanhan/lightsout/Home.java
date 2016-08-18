4ccea9f57580f6aa71fec6f4b29b86fb349abdfe
David Wu <fastbyte22@protonmail.com>
2016-08-18 10:46:29 +0200

    created Home Activity

---
+++ app/src/main/java/com/jimdo/hanhan/lightsout/Home.java
@@ -0,0 +1,14 @@
+package com.jimdo.hanhan.lightsout;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
