4040b8c399bc75f46325571b92bea058ae6ca3d5
David Wu <fastbyte22@protonmail.com>
2016-08-18 11:06:12 +0200

    set onclicklistener for btnStart

--- app/src/main/java/com/jimdo/hanhan/lightsout/Home.java
+++ app/src/main/java/com/jimdo/hanhan/lightsout/Home.java
@@ -1,7 +1,10 @@
 package com.jimdo.hanhan.lightsout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends Activity {
 
@@ -9,6 +12,16 @@ public class Home extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btnStart=(Button) findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),MainLevel.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
