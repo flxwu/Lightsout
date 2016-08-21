20e6d8da6fdca4eeefdfecba407dcf581c659a2c
FastByte-22 <fastbyte22@protonmail.com>
2016-08-21 14:05:02 +0200

    added Next Level button

--- app/src/main/java/com/jimdo/hanhan/lightsout/WinScreen.java
+++ app/src/main/java/com/jimdo/hanhan/lightsout/WinScreen.java
@@ -7,7 +7,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import com.jimdo.hanhan.lightsout.tools.Level;
+
+/**
+ * Created by David Wu on 18.08.2016(c)
+ *
+ */
 public class WinScreen extends Activity {
+    protected Level level;
+
+    public WinScreen() {
+
+    }
+    public WinScreen(Level level) {
+        this.level=level;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,22 +31,32 @@ public class WinScreen extends Activity {
 
         RelativeLayout layout = (RelativeLayout) findViewById(R.id.layout);
 
-        Button btnTryAgain = (Button) findViewById(R.id.btnTryAgain);
         Button btnHome = (Button) findViewById(R.id.btnHome);
-
-        btnTryAgain.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainLevel.class);
-                intent.putExtra("level",1);
-                startActivity(intent);
-            }
-        });
+        Button btnNextLevel = (Button) findViewById(R.id.btnNextLevel);
+        Button btnTryAgain = (Button) findViewById(R.id.btnTryAgain);
 
         btnHome.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+            }
+        });
+
+        btnNextLevel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), MainLevel.class);
+                intent.putExtra("level",level.getNextLevel());
+                startActivity(intent);
+            }
+        });
+
+        btnTryAgain.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainLevel.class);
+                intent.putExtra("level", level.getLevel());
                 startActivity(intent);
             }
         });
